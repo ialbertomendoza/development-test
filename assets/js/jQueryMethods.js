d502efbe4d656e99a00591ef7fb3f4e464c4dc43
Neris <mendozaene@gmail.com>
2016-07-24 21:16:37 -0500

    Debe existir al menus 1 cliente y 1 parker para iniciar la prueba

--- assets/js/jQueryMethods.js
+++ assets/js/jQueryMethods.js
@@ -6,10 +6,14 @@ $("#simulator-start").on("click", function(){
 	resetMarkers();
 	var parkers = $("#input-parkers").val();
 	var customers = $("#input-customers").val();
-	markerGenerator("parker", parkers);
-	markerGenerator("customer", customers);
-	$(this).addClass("disabled");
-	$(".control").removeClass("disabled");
+	if (parkers && parkers > 0 && customers && customers > 0) {
+		markerGenerator("parker", parkers);
+		markerGenerator("customer", customers);
+		$(this).addClass("disabled");
+		$(".control").removeClass("disabled");
+	} else {
+		Materialize.toast('¡No hay datos para realizar la prueba!', 2000);
+	}
 });
 
 $("#simulator-pause").on("click", function(){
